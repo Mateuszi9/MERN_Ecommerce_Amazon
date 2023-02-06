1547944fdb2ce6b1e597af868359f5a4368da5a9
Mateusz Izdebski <mati2k51@gmail.com>
2023-02-06 13:04:21 +0100

    12-crearte-product-details-screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
 
 //define reducer function (current state, action)
 const reducer = (state, action) => {
@@ -52,6 +53,9 @@ function HomeScreen() {
 
   return (
     <div>
+      <Helmet>
+        <title>Amazon</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
